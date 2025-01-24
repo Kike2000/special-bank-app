84af7724be96c6e7b4c541e40fe31cbbb0b52bd3
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2025-01-23 22:22:16 -0600

    feat: add api to project

---
+++ SpecialBankBackend/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpecialBankAPI.Models
+{
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TransactionUniqueReference { get; set; }
+        public decimal TransactionAmount { get; set; }
+        public TransactionStatus TransactionStatus { get; set; }
+        public bool IsSuccesful => TransactionStatus.Equals(TransactionStatus.Success);
+        public string TransactionSourceAccount { get; set; }
+        public string TransactionDestinationAccount { get; set; }
+  
+        public string TransactionParticulars { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public DateTime? TransactionDate { get; set; }
+    }
+    public enum TransactionStatus
+    {
+        Success,
+        Failed,
+        Error
+    }
+
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
+    }
+
+
+}
